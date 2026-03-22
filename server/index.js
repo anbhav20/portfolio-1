68d91a33c58ee6264db8ee6a44765e3794ddae26
anbhav <your.email@example.com>
2026-03-22 14:28:33 +0530

    fix server error.

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // ✅ 🔥 FIX: Serve built frontend + assets correctly
 app.use(
-  express.static(path.join(__dirname, "../dist/"))
+  express.static(path.join(__dirname, "../dist/public/"))
 );
 
 // Request logger middleware
